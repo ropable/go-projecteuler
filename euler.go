4f1489e0bdb1f463586c0a6bc73664de93aacedf
Ashley Felton <ashley@ropable.com>
2014-12-08 11:41:14 +0800

    Better solution 1. Added gitignore.

--- euler.go
+++ euler.go
@@ -16,9 +16,23 @@ func main() {
         os.Exit(1)
     }
     problemNo := os.Args[1]
-    problemOne()
+    if (problemNo == "1") {
+        problemOne()
+    }
 }
 
 func problemOne() {
-    fmt.Println(math.Mod(13, 4))
+    // Problem 1
+    // Multiples of 3 and 5
+    // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+    // The sum of these multiples is 23.
+    // Find the sum of all the multiples of 3 or 5 below 1000.
+    sum := 0.0
+    // Loop from 1 to 999; if i modulo 3 or i modulo 5 is 0, add i to the sum.
+    for i := 1.0; i < 1000.0; i++ {
+        if (math.Mod(i, 3.0) == 0 || math.Mod(i, 5.0) == 0) {
+            sum = sum + i
+        }
+    }
+    fmt.Println(int(sum))
 }
